292d168da728b8ffc130b7be220d8c8fbcfed80e
clementbedour <bedour@insa-toulouse.fr>
2026-03-20 21:10:10 +0100

    ajout -e et -r + commentaires

--- tsockv4.c
+++ tsockv4.c
@@ -24,7 +24,7 @@ void construire_message(char *message, char motif, int lg, int num_msg)
 	char prefix[6];
 	int i;
 
-	// Ajout du motif de dÃ©part de message
+	// Ajout du motif de depart de message
 	snprintf(prefix, sizeof(prefix), "%5d", num_msg);
 	for (i = 0; i < 5; i++)
 	{
@@ -44,13 +44,13 @@ void construire_message(char *message, char motif, int lg, int num_msg)
 
 int main(int argc, char **argv)
 {
-	// Inititialisation des variables utilisÃ©s du cotÃ© UDP / TCP /
+	// Inititialisation des variables utilises du cote UDP / TCP /
 	int c;
 	extern char *optarg;
 	extern int optind;
-	int nb_message = -1; // Nb de messages Ã  envoyer ou Ã  recevoir, par dÃ©faut : 10 en Ã©mission, infini en rÃ©ception
+	int nb_message = -1; // Nb de messages a  envoyer ou a  recevoir, par defaut : 10 en emission, infini en reception
 	int source = -1;	 // 0 = puits, 1=source
-	int l = 30;			 // longueur max Ã©mis ou reÃ§u de base Ã  30 octets
+	int l = 30;			 // longueur max emis ou recu de base a  30 octets
 	int u = 0;			 // 0 = TCP, 1=UDP
 	int sock, sock_bis;
 	struct sockaddr_in adr_distant;
@@ -58,22 +58,25 @@ int main(int argc, char **argv)
 	socklen_t lg_adr_distant = sizeof(adr_distant);
 	socklen_t lg_adr_local = sizeof(adr_local);
 	int lg_M = 30;
-	int lg_emis = 0;
+	int lg_emis;
 	struct hostent *IP;
 	int lg_rec;
 	int lu;		 // nbr message lu par UDP
-	int lus = 0; // nbr message lu par TCP
-	// Variable par dÃ©fault si pas dÃ©fini dans les paramÃ¨tres
+	int lus = 0; // nbr message lu avec methode pere/fil
+	// Variable par default si pas defini dans les parametres
 	char *nom = "localhost";
 	int port = 9000;
 
-	// RÃ©cupÃ©ration des options rentrÃ©es en entrÃ©es de la fonction
+	// Recuperation des options rentrées en entrees de la fonction
 
-	while ((c = getopt(argc, argv, "upn:sl:")) != -1)
+	//-r recepteur des donnees
+	//-e emetteur des donnees
+
+	while ((c = getopt(argc, argv, "upren:sl:")) != -1)
 	{
 		switch (c)
 		{
-		case 'p':
+		case 'p': //puit
 			if (source == 1)
 			{
 				printf("usage: cmd [-p|-s][-n ##]\n");
@@ -81,10 +84,20 @@ int main(int argc, char **argv)
 			}
 			source = 0;
 			break;
+		
+		case 'r': //recepteur = puit
+			if (source == 1)
+			{
+				exit(1);
+			}
+			source = 0;
+			break;
+		
+		
 		case 'u':
 			u = 1;
 			break;
-		case 's':
+		case 's'://source
 			if (source == 0)
 			{
 				printf("usage: cmd [-p|-s][-n ##]\n");
@@ -92,6 +105,15 @@ int main(int argc, char **argv)
 			}
 			source = 1;
 			break;
+
+			case 'e'://emetteur = source
+			if (source == 0)
+			{
+				exit(1);
+			}
+			source = 1;
+			break;
+
 		case 'n':
 			nb_message = atoi(optarg);
 			break;
@@ -106,9 +128,9 @@ int main(int argc, char **argv)
 	}
 
 	char *M = malloc(sizeof(char) * lg_M);
-	char *pmesg = malloc(sizeof(char) * lg_M); // Allocation dynamique pour s'adapter Ã  l'option -l
+	char *pmesg = malloc(sizeof(char) * lg_M); // Allocation dynamique pour s'adapter a  l'option -l
 
-	// RÃ©cupÃ©ration du nom de domaine et du numÃ©ro de port
+	// Recuperation du nom de domaine et du numero de port
 
 	if (optind != argc)
 	{
@@ -127,7 +149,7 @@ int main(int argc, char **argv)
 			port = atoi(argv[optind]);
 		}
 	}
-	// Emplacement initial des fonctions permettant de vÃ©rifier si on est en source / puit etc
+	// Emplacement initial des fonctions permettant de verifier si on est en source / puit etc
 	if (source == -1)
 	{
 		printf("usage: cmd [-p|-s][-n ##]\n");
@@ -141,10 +163,10 @@ int main(int argc, char **argv)
 
 	//-------------SOURCE SOUS UDP------------------------------------------
 	if (u == 1 && source == 1)
-	{ // crÃ©ation socket pour source
+	{ // creation socket pour source
 		if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 		{
-			printf("echec crÃ©ation socket dans source \n");
+			printf("echec creation socket dans source \n");
 			exit(1);
 		}
 		/* reset de la structure */
@@ -152,7 +174,7 @@ int main(int argc, char **argv)
 		adr_distant.sin_family = AF_INET;
 		adr_distant.sin_port = port;
 
-		// RÃ©cupÃ©ration de l'IP correspondant au nom de domaine
+		//Recuperation de l'IP correspondant au nom de domaine
 
 		if ((IP = gethostbyname(nom)) == NULL)
 		{
@@ -166,17 +188,25 @@ int main(int argc, char **argv)
 		{
 			construire_message(M, 'a' + (i % 26), lg_M, i + 1);
 			lg_emis = sendto(sock, M, lg_M, 0, (struct sockaddr *)&adr_distant, lg_adr_distant);
-			printf("SOURCE: Envoi nÂ°%d (%d) [%.*s]\n", i + 1, lg_M, lg_M, M);
+			if (lg_emis ==-1){
+				printf("Probleme sendto dans source UDP\n");
+			}
+			if (lg_emis ==0){
+				printf("SOURCE: Envoi n°%d le message n'a pas été envoyé car la taille est de 0 \n",i+1);
+			}
+			else {
+				printf("SOURCE: Envoi n°%d (%d) [%.*s]\n", i + 1, lg_M, lg_M, M);
+			}
 		}
 		printf("SOURCE: fin\n");
 	}
 
 	//--------------SOURCE SOUS TCP------------------------------------------
 	if (u == 0 && source == 1)
-	{ // crÃ©ation socket pour source
+	{ // creation socket pour source
 		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		{
-			printf("echec crÃ©ation socket dans source TCP\n");
+			printf("echec creation socket dans source TCP\n");
 			exit(1);
 		}
 		// init et reset de la structure
@@ -205,14 +235,21 @@ int main(int argc, char **argv)
 			construire_message(M, 'a' + (i % 26), lg_M, i + 1);
 			// ou send(sock, M, lg_M, 0) mais pas sendto car pour UDP code de la V1 inutile ici
 			lg_emis = write(sock, M, lg_M);
-			printf("SOURCE: Envoi nÂ°%d (%d) [%.*s]\n", i + 1, lg_M, lg_M, M);
+			if (lg_emis==-1){
+				printf("Erreur write dans source TCP \n");
+			}
+			if (lg_emis==0){
+				printf("SOURCE: Envoi n°%d n'a envoyé aucun message \n", i + 1);
+			}
+			printf("SOURCE: Envoi n°%d (%d) [%.*s]\n", i + 1, lg_M, lg_M, M);
 		}
 		printf("SOURCE: fin\n");
-		if (shutdown(sock, 1) == -1)
+
+		if (shutdown(sock, 1) == -1) //fin de connexion car plus de msg à envoyé
 		{
-			printf("ProblÃ¨me fin de connexion dans la source\n");
+			printf("Probleme fin de connexion dans la source\n");
 		}
-		close(sock);
+		close(sock); //destruction du socket
 	}
 
 	//--------------PUITS SOUS UDP------------------------------------------
@@ -222,10 +259,10 @@ int main(int argc, char **argv)
 			printf("PUITS: lg_mesg-lu=%d, port=%d, nb_receptions=infini, TP=udp\n", lg_M, port);
 		else
 			printf("PUITS: lg_mesg-lu=%d, port=%d, nb_receptions=%d, TP=udp\n", lg_M, port, nb_message);
-		// crÃ©ation socket pour puits
+		// creation socket pour puits
 		if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 		{
-			printf("echec crÃ©ation socket dans puits \n");
+			printf("echec creation socket dans puits \n");
 			exit(1);
 		}
 
@@ -236,22 +273,25 @@ int main(int argc, char **argv)
 		// association adresse socket
 		if (bind(sock, (struct sockaddr *)&adr_local, lg_adr_local) == -1)
 		{
-			printf("Ã©chec du bind\n");
+			printf("echec du bind\n");
 			exit(1);
 		}
 
-		// mis en Ã©coute
-		// listen(sock,20); Pas nÃ©cessaire, seulement pour TCP
-		// faisait partie de la V1 et pas nÃ©cessaire
+		// mis en ecoute
+		// listen(sock,20); Pas necessaire, seulement pour TCP
+		// faisait partie de la V1 et pas necessaire
 
 		// lecture infini
 		int nb_m = 1;
 		while (nb_message == -1 || nb_m <= nb_message)
 		{
 			lu = recvfrom(sock, pmesg, lg_M, 0, (struct sockaddr *)&adr_distant, &lg_adr_distant);
+			if (lu ==-1){
+				printf("Erreur dans le puit de udp a cause de recvfrom \n");
+			}
 			if (lu > 0)
 			{
-				printf("PUITS: Reception nÂ°%d (%d) [%.*s]\n", nb_m, lu, lu, pmesg);
+				printf("PUITS: Reception n°%d (%d) [%.*s]\n", nb_m, lu, lu, pmesg);
 				nb_m++;
 			}
 		}
@@ -264,10 +304,10 @@ int main(int argc, char **argv)
 			printf("PUITS: lg_mesg-lu=%d, port=%d, nb_receptions=infini, TP=tcp\n", lg_M, port);
 		else
 			printf("PUITS: lg_mesg-lu=%d, port=%d, nb_receptions=%d, TP=tcp\n", lg_M, port, nb_message);
-		// crÃ©ation socket pour puits
+		// creation socket pour puits
 		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		{
-			printf("echec crÃ©ation socket dans puits \n");
+			printf("echec creation socket dans puits \n");
 			exit(1);
 		}
 
@@ -279,36 +319,37 @@ int main(int argc, char **argv)
 		// association adresse socket
 		if (bind(sock, (struct sockaddr *)&adr_local, lg_adr_local) == -1)
 		{
-			printf("Ã©chec du bind\n");
+			printf("echec du bind\n");
 			exit(1);
 		}
 
-		// mis en Ã©coute
+		// mis en ecoute
 		listen(sock, 5);
 
-		// Mise en place de la crÃ©ation de nouveaux processus si nÃ©cÃ©ssaire
+		// Mise en place de la creation de nouveaux processus si necessaire
 		while (1)
 		{ // acceptation de la connexion
 			if ((sock_bis = accept(sock, (struct sockaddr *)&adr_distant, &lg_adr_distant)) == -1)
 			{
-				printf("Ã©chec du accept\n");
+				printf("echec du accept\n");
 				continue; // On passe au client suivant en cas d'erreur
 			}
 
-			// CrÃ©ation d'un processus fils pour gÃ©rer ce client spÃ©cifiquement
+			// Creation d'un processus fils pour gerer ce client specifiquement
 			int pid;
 
 			switch (pid = fork())
 			{
 			case -1:
 				// ERREUR
-				printf("Ã©chec du fork\n");
-				close(sock_bis);
+				printf("echec du fork\n");
+				shutdown(sock_bis, 0); //ferme la connexion
+				close(sock_bis);//detruit le socket
 				break;
 
 			case 0:
 				// PROCESSUS FILS : S'occupe de la lecture
-				close(sock); // Le fils n'a pas besoin du sock d'Ã©coute
+				close(sock); // Le fils n'a pas besoin du sock d'ecoute
 
 				lus = 0;
 				while (nb_message == -1 || lus < nb_message)
@@ -317,18 +358,18 @@ int main(int argc, char **argv)
 					{
 						break;
 					}
-					printf("PUITS: Reception nÂ°%d (%d) [%.*s]\n", lus + 1, lg_rec, lg_rec, M);
+					printf("PUITS: Reception n°%d (%d) [%.*s]\n", lus + 1, lg_rec, lg_rec, M);
 					lus++;
 				}
 
-				// Fermeture propre : on coupe la lecture, puis on dÃ©truit le socket
+				// Fermeture propre : on coupe la lecture, puis on detruit le socket
 				shutdown(sock_bis, 0);
-				close(sock_bis); // Le fils ferme son socket de dialogue une fois terminÃ©
+				close(sock_bis); // Le fils ferme son socket de dialogue une fois termine
 				exit(0);
 
 			default:
-				// PROCESSUS PERE : Continue d'Ã©couter
-				close(sock_bis); // Le pÃ¨re n'a pas besoin du sock de dialogue
+				// PROCESSUS PERE : Continue d'ecouter
+				close(sock_bis); // Le pere n'a pas besoin du sock de dialogue
 				break;
 			}
 		}
